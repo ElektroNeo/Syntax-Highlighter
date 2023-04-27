dae9ae519a7dd8651c62a29aa0afc05a4bc96f4d
Bahtiyar Bayram <bahtiyar.bayramov@gmail.com>
2023-04-28 00:25:52 +0300

    Upload files.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Syntax-Highlighter
+# Syntax Highlighter
+
 Highlight syntax and generate html file that can be used with PrismJS.
+
+## Initialisation
+
+Install required packages using `npm install`.
+
+## Usage
+
+- Add code that you want to format in `code.c` file.
+- Open terminal in the project directory.
+- Run `node .\sh.js [language]`.
+- Code will be generated to the `highlighted-code.html` file and also will be copied to the clipboard.
+
+## Supported Languages
+
+Currently those languages supported.
+- c
+- cpp
+- sh
+- rust
+- python
+- csharp
---
+++ sh.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const ncp = require("copy-paste");
+const Prism = require("prismjs");
+const loadLanguages = require("prismjs/components/");
+
+let prism_lang_str;
+
+if (process.argv.length > 2) {
+  prism_lang_str = process.argv[2].toString();
+} else {
+  prism_lang_str = "c";
+}
+
+loadLanguages([prism_lang_str]);
+
+const before_code = `<!-- ${prism_lang_str.toUpperCase()} style sheets for Prism.js -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css"
+  rel="stylesheet"
+/>
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/line-numbers/prism-line-numbers.min.css"
+  rel="stylesheet"
+/>
+<button class="copy-to-clipboard-button" type="button" data-copy-state="copy">
+	<span>Copy</span>
+</button>
+<!-- Code snippet content -->
+<pre class="line-numbers"><code class="language-${prism_lang_str}">`;
+
+const after_code = `</code>
+
+</pre>
+<!-- Javascript for Prism.js core, line number display, and ${prism_lang_str} syntax highlight -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/line-numbers/prism-line-numbers.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-${prism_lang_str}.min.js"></script>`;
+
+// The code snippet you want to highlight, as a string
+const code = fs.readFileSync("./code.c").toString();
+
+let prism_lang;
+
+switch (prism_lang_str) {
+  case "c":
+    prism_lang = Prism.languages.c;
+    break;
+  case "cpp":
+    prism_lang = Prism.languages.cpp;
+    break;
+  case "sh":
+    prism_lang = Prism.languages.bash;
+    break;
+  case "js":
+    prism_lang = Prism.languages.js;
+    break;
+  case "rust":
+    prism_lang = Prism.languages.rust;
+    break;
+  case "python":
+    prism_lang = Prism.languages.python;
+    break;
+  case "csharp":
+    prism_lang = Prism.languages.csharp;
+    break;
+  default:
+    prism_lang = Prism.languages.c;
+    break;
+}
+
+// Returns a highlighted HTML string
+const html = Prism.highlight(code, prism_lang, prism_lang_str);
+
+let final_code = before_code + html + after_code;
+
+fs.writeFileSync("highlighted-code.html", final_code);
+
+// Copy to clipboard
+ncp.copy(final_code, function () {
+  console.log("Code generated and copied to the clipboard!");
+});
